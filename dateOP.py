58884c47f2619d5b153074494b47c864517b336c
AsmaaFelemban <Asmaakf@hotmail.com>
2024-02-13 00:12:32 +0300

    Solved lab modules and packages

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+def current_date():
+    '''This function to print the current date by using date module'''
+    today_date = date.today()
+    print(f"Current date: {today_date}") 
+
