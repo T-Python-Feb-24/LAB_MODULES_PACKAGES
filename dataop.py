83b3a7210769303b01826b71474b58afca5ba615
jmailnaif <jamailotb15@gmail.com>
2024-02-12 23:14:40 +0300

    lab modules

---
+++ dataop.py
@@ -0,0 +1,8 @@
+from datatine import date
+def today_s ():
+    today = date.today()
+    print("today date is :", today)
+    
+import dataop
+current_date =dataop.current_date()
+print(current_date)
