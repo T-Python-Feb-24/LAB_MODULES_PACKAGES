21bbbfafd0096352cd800b15f2748e2ef89ab026
Mohammed Alyahya <modezdf@gmail.com>
2024-02-12 19:43:31 +0300

    solved the Modules & Packages

---
+++ dateOP.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+def date_for_today():
+    current_date = date.today()
+
+    print(current_date)
+    
+ 
+ 
+ 
+date_for_today()
---
+++ main.py
@@ -0,0 +1,4 @@
+import dateOP
+from dateOP import date_for_today
+
+print(date_for_today)
